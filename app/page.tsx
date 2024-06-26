8790cf1bee5f38dc97afa26dc5abcd6a5732ddbe
h6mzy <112537750+h6mzy@users.noreply.github.com>
2024-06-26 01:13:09 +0000

    switch nav gradient

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <main>
       <Affix offsetTop={0}>
-        <div className={navStyles.bar}>
+        <div className={navStyles.barGradient}>
           <Space
             block
             align='center'
