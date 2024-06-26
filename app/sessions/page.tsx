a9c32d4bf6c90b660eee855a8cf45d31840e3470
h6mzy <112537750+h6mzy@users.noreply.github.com>
2024-06-26 08:00:58 +0000

    wip

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 
-import { List } from 'antd-mobile'
+import { Card, List, Swiper } from 'antd-mobile'
 import { sessions } from '../_lib/sessions'
+import FixedAspectRatio from '../components/fixed-aspect-ratio'
+import { players } from '../_lib/players'
 
 export default function Sessions() {
+
+  function getPlayerName(num:number) {
+    return players.find(p => p.squad_number === num)?.name || ''
+  }
+
   return (
     <main>
       <div className='pad'>
@@ -11,7 +18,16 @@ export default function Sessions() {
         <List>
           {sessions.map((session, sessionIndex) =>
             <List.Item key={sessionIndex}>
-              {session.players.join(', ')}
+              <Swiper stuckAtBoundary={false} slideSize={30}>
+                {session.players.map((player_num, playerIndex) =>
+                  <Swiper.Item key={playerIndex}>
+                    <Card style={{ border: '1px solid var(--adm-color-primary)' }}>
+                    <FixedAspectRatio>
+                      {getPlayerName(player_num)}
+                    </FixedAspectRatio></Card>
+                  </Swiper.Item>
+                )}
+              </Swiper>
             </List.Item>
           )}
         </List>
