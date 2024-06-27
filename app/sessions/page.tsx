4c7866b85d7d9c9aeaeded6473dd278526245143
h6mzy <112537750+h6mzy@users.noreply.github.com>
2024-06-27 07:45:51 +0000

    sessions page

--- app/sessions/page.tsx
+++ app/sessions/page.tsx
@@ -1,45 +1,63 @@
 'use client'
 
-import { Card, List, Swiper } from 'antd-mobile'
+import { Image, List, Space, Swiper } from 'antd-mobile'
 import { sessions } from '../_lib/sessions'
 import FixedAspectRatio from '../components/fixed-aspect-ratio'
 import { players } from '../_lib/players'
 import AffixedBar from '../components/affixed-bar'
+import dayjs from 'dayjs'
+import styles from './session.module.sass'
 
 export default function Sessions() {
 
-  function getPlayerName(num:number) {
-    return players.find(p => p.squad_number === num)?.name || ''
+  function getPlayer(num:number) {
+    return players.find(p => p.squad_number === num)
   }
 
   return (
     <main>
       <AffixedBar label='Next session' />
-      <div className='pad'>
-        <h1 className='text-center'>Sessions</h1>
-        <List
-          style={{ 
-            '--border-top': '0px',
-            '--border-bottom': '0px',
-            '--padding-left': '0px',
-            '--padding-right': '0px',
-          }}
-        >
-          {sessions.map((session, sessionIndex) =>
-            <List.Item key={sessionIndex}>
-              <Swiper stuckAtBoundary={false} slideSize={30}>
-                {session.players.map((player_num, playerIndex) =>
+      <h1 className='text-center'>Sessions</h1>
+      <div>
+        {sessions.map((session, sessionIndex) =>
+          <Space
+            block
+            justify='stretch'
+            className={styles.row}
+            style={{ '--gap': '0px' }}
+            key={sessionIndex}
+          >
+            <div className={styles.card} style={{ background: 'var(--adm-color-background)', zIndex: 1, position: 'relative' }}>
+              <FixedAspectRatio ratio={[4,5]}>
+                <div>
+                {dayjs(session.date).format('D')}<br />
+                {dayjs(session.date).format('MMM')}</div>
+              </FixedAspectRatio>
+            </div>
+            <Swiper
+              //stuckAtBoundary={false}
+              slideSize={33.33}
+              indicator={() => null}
+              style={{ overflow: 'visible' }}
+            >
+              {session.players.map((player_num, playerIndex) => {
+                const player:any = getPlayer(player_num)
+                const { name, photo, squad_number } = player
+                return (
                   <Swiper.Item key={playerIndex}>
-                    <Card style={{ border: '1px solid var(--adm-color-primary)' }}>
-                    <FixedAspectRatio>
-                      {getPlayerName(player_num)}
-                    </FixedAspectRatio></Card>
+                    <div className={styles.card}>
+                      <FixedAspectRatio ratio={[4,5]}>
+                        <div className={styles.number}>{squad_number}</div>
+                        <Image src={photo} fit='cover' style={{ '--height': '100%', zIndex: 1 }} />
+                        <div className={styles.name}>{name}</div>
+                      </FixedAspectRatio>
+                    </div>
                   </Swiper.Item>
-                )}
-              </Swiper>
-            </List.Item>
-          )}
-        </List>
+                )
+              })}
+            </Swiper>
+          </Space>
+        )}
       </div>
     </main>
   )
