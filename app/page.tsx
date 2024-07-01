ce4a4e575d7571a67a284e80ec8f900bee6a7cba
Hamzy <112537750+hamzyselamat@users.noreply.github.com>
2024-07-01 14:38:57 +0800

    update antd-mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
 'use client'
 
-import { List } from 'antd-mobile'
+import { Grid, List } from 'antd-mobile'
 import { players } from './_lib/players'
 import styles from './components/lineup.module.sass'
 import PlayerCard from './components/player-card'
 import AffixedBar from './components/affixed-bar'
+import SquadCard from './components/squad-card'
 
 export default function Home() {
   return (
     <main>
       <AffixedBar label='Kick off' />
+      <div className='pad'>
+        <p className='color-primary text-center'>Confirmed Attendees</p>
+        <Grid columns={3} gap='var(--adm-gap-sm)'>
+          {players.map((player, playerIndex) => (
+            <Grid.Item key={playerIndex}>
+              <SquadCard player={player} />
+            </Grid.Item>
+          ))}
+        </Grid>
+        <p className='color-weak text-center'>Unavailable</p>
+        <Grid columns={3} gap='var(--adm-gap-sm)' style={{ filter: 'grayscale(1)', opacity: .65 }}>
+          {players.map((player, playerIndex) => (
+            <Grid.Item key={playerIndex}>
+              <SquadCard player={player} />
+            </Grid.Item>
+          ))}
+        </Grid>
+      </div>
+      {/*
       <List
         className={styles.lineup}
         style={{ 
@@ -25,6 +45,7 @@ export default function Home() {
           )
         })}
       </List>
+      */}
     </main>
   )
 }
